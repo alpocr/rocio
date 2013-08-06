158b59777a380a916819d87fdc4b583193862d5b
Allan <alpocr@4geeks.info>
2013-08-05 22:58:02 -0600

    renombre de carpeta models

---
+++ src/net/ulatina/rocio/models/Function.java
@@ -0,0 +1,78 @@
+package net.ulatina.rocio.models;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Set;
+
+
+/**
+ * The persistent class for the function database table.
+ * 
+ */
+@Entity
+@Table(name="function")
+public class Function implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+
+	private String name;
+
+	private String path;
+
+	//bi-directional many-to-one association to FunctionHasModule
+	@OneToMany(mappedBy="function")
+	private Set<FunctionHasModule> functionHasModules;
+
+	public Function() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPath() {
+		return this.path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public Set<FunctionHasModule> getFunctionHasModules() {
+		return this.functionHasModules;
+	}
+
+	public void setFunctionHasModules(Set<FunctionHasModule> functionHasModules) {
+		this.functionHasModules = functionHasModules;
+	}
+
+	public FunctionHasModule addFunctionHasModule(FunctionHasModule functionHasModule) {
+		getFunctionHasModules().add(functionHasModule);
+		functionHasModule.setFunction(this);
+
+		return functionHasModule;
+	}
+
+	public FunctionHasModule removeFunctionHasModule(FunctionHasModule functionHasModule) {
+		getFunctionHasModules().remove(functionHasModule);
+		functionHasModule.setFunction(null);
+
+		return functionHasModule;
+	}
+
+}
---
+++ src/net/ulatina/rocio/models/Module.java
@@ -0,0 +1,94 @@
+package net.ulatina.rocio.models;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Set;
+
+
+/**
+ * The persistent class for the module database table.
+ * 
+ */
+@Entity
+@Table(name="module")
+public class Module implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+
+	private String name;
+
+	//bi-directional many-to-one association to FunctionHasModule
+	@OneToMany(mappedBy="module")
+	private Set<FunctionHasModule> functionHasModules;
+
+	//bi-directional many-to-one association to ModuleHasApp
+	@OneToMany(mappedBy="module")
+	private Set<ModuleHasApp> moduleHasApps;
+
+	public Module() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<FunctionHasModule> getFunctionHasModules() {
+		return this.functionHasModules;
+	}
+
+	public void setFunctionHasModules(Set<FunctionHasModule> functionHasModules) {
+		this.functionHasModules = functionHasModules;
+	}
+
+	public FunctionHasModule addFunctionHasModule(FunctionHasModule functionHasModule) {
+		getFunctionHasModules().add(functionHasModule);
+		functionHasModule.setModule(this);
+
+		return functionHasModule;
+	}
+
+	public FunctionHasModule removeFunctionHasModule(FunctionHasModule functionHasModule) {
+		getFunctionHasModules().remove(functionHasModule);
+		functionHasModule.setModule(null);
+
+		return functionHasModule;
+	}
+
+	public Set<ModuleHasApp> getModuleHasApps() {
+		return this.moduleHasApps;
+	}
+
+	public void setModuleHasApps(Set<ModuleHasApp> moduleHasApps) {
+		this.moduleHasApps = moduleHasApps;
+	}
+
+	public ModuleHasApp addModuleHasApp(ModuleHasApp moduleHasApp) {
+		getModuleHasApps().add(moduleHasApp);
+		moduleHasApp.setModule(this);
+
+		return moduleHasApp;
+	}
+
+	public ModuleHasApp removeModuleHasApp(ModuleHasApp moduleHasApp) {
+		getModuleHasApps().remove(moduleHasApp);
+		moduleHasApp.setModule(null);
+
+		return moduleHasApp;
+	}
+
+}
